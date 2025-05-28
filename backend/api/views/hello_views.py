0adf973ba3471cbe833c1f8539a9ee2cbfd14d03
heo <admin@admin.com>
2025-05-28 17:26:10 +0900

    commit

--- backend/api/views/hello_views.py
+++ backend/api/views/hello_views.py
@@ -2,7 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 #dev_4
+#dev_5_수정
 class HelloWorldView(APIView):
     def get(self, request):
         return Response({"message": "Hello, World"})
+
 
