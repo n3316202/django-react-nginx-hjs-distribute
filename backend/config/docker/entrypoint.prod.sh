b003aa5e8b4bc7444a28f8a6429a7cad38a39199
heo <admin@admin.com>
2025-05-29 22:11:27 +0900

    commit

--- backend/config/docker/entrypoint.prod.sh
+++ backend/config/docker/entrypoint.prod.sh
@@ -17,7 +17,7 @@ echo "📌 엔트리포인트 스크립트 시작"
 # ✅ Django 마이그레이션 및 static 파일 수집
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-#python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # ✅ 명령어 실행
 exec "$@"
