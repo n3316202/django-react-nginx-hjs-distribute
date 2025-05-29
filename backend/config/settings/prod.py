e34bb93f63a37be0e0de26974f6beff9ec7ad79a
heo <admin@admin.com>
2025-05-29 19:02:02 +0900

    commit

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -2,20 +2,26 @@
 # 배포환경
 #pip install python-decouple
 
+# from .base import *
+# from decouple import config
+
+# DEBUG = config('DEBUG', default=False, cast=bool)
+
+# #SECRET_KEY = config('DJANGO_SECRET_KEY')
+
+# ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS').split(',')
+
+# CORS_ALLOWED_ORIGINS = [
+#     config('CORS_ALLOWED_ORIGIN'),
+# ]
+
+# CSRF_TRUSTED_ORIGINS = [
+#     config('CSRF_TRUSTED_ORIGIN'),
+# ]
+
 from .base import *
-from decouple import config
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
 
-#SECRET_KEY = config('DJANGO_SECRET_KEY')
-
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS').split(',')
-
-CORS_ALLOWED_ORIGINS = [
-    config('CORS_ALLOWED_ORIGIN'),
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    config('CSRF_TRUSTED_ORIGIN'),
-]
 
