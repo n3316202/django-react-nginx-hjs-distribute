9793e90e06c2e92c2d6dced3411461e08812b3f7
heo <admin@admin.com>
2025-05-30 22:11:56 +0900

    commit

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -8,14 +8,14 @@ DEBUG = True
 #도메인에서 오는 요청만 Django가 처리
 ALLOWED_HOSTS = ["*"]
 
-CORS_ORIGIN_ALLOW_ALL = True  # 어떠한 출처든 상관없이 정보를 공유
+# CORS_ORIGIN_ALLOW_ALL = True  # 어떠한 출처든 상관없이 정보를 공유
 
-# 다른 도메인에서의 API 접근 허용
-# 정확한 주소 필요, 포트 포함
-# CORS_ORIGIN_ALLOW_ALL = True 이면 아래는 필요 없음
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",  # 프론트 도메인
-]
+# # 다른 도메인에서의 API 접근 허용
+# # 정확한 주소 필요, 포트 포함
+# # CORS_ORIGIN_ALLOW_ALL = True 이면 아래는 필요 없음
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://localhost:5173",  # 프론트 도메인
+# ]
 
