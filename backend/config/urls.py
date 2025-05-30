686973c131ecdbf34ccc2743929da0904579e66c
heo <admin@admin.com>
2025-05-31 03:24:42 +0900

    commit

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,7 +5,6 @@ from django.urls import path, include #dev_4
 from .views import main_page  # dev_4
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api/', include('api.urls')),  #dev_4
-    path('/', include('api.urls')),  #dev_6
+    path('api/', include('api.urls')),  #dev_4
     path('', main_page, name='main'),  # 메인 페이지로 연결
 ]
