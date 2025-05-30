b51c2a5942d04b9b6c18d9c8faad994493db1d05
heo <admin@admin.com>
2025-05-31 01:52:14 +0900

    commit

--- frontend/src/api/CategoryApi.js
+++ frontend/src/api/CategoryApi.js
@@ -3,9 +3,9 @@ import http from './HttpCommon';
 //#dev_5
 //http://127.0.0.1:8000/api/categories/
 export const getCategories = () => {
-  return http.get('/api/categories/');
+  return http.get('/categories/');
 };
 
 export const getCategoryById = (id) => {
-  return http.get(`/api/categories/${id}/`);
+  return http.get(`/categories/${id}/`);
 };
