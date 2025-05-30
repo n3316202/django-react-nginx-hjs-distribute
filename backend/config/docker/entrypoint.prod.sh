ca913214de51df2cdb9ebbb7f7873d2635663a80
heo <admin@admin.com>
2025-05-31 03:46:01 +0900

    commit

--- backend/config/docker/entrypoint.prod.sh
+++ backend/config/docker/entrypoint.prod.sh
@@ -21,7 +21,7 @@ echo "$DJANGO_ENV_PROD_CONTENTS" > /app/backend/.env.prod
 # ✅ Django 마이그레이션 및 static 파일 수집
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-#python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input #dev_6_2
 
 # ✅ 명령어 실행
 exec "$@"
--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -21,3 +21,19 @@ from decouple import config
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
+
+#dev_6_2
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 정적파일 URL 경로
+STATIC_URL = '/static/'
+
+# collectstatic 명령어로 모이는 폴더 (EC2 내 정적파일 모음 경로)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# (개발 중에만) 추가 정적파일 경로 지정 가능
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
