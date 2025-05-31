b2afe7f9f30456ba764591119e82e47078831aac
heo <admin@admin.com>
2025-05-31 13:16:23 +0900

    commit

--- backend/config/docker/entrypoint.prod.sh
+++ backend/config/docker/entrypoint.prod.sh
@@ -18,10 +18,18 @@ echo "📌 엔트리포인트 스크립트 시작"
 echo "🔧 .env.prod 파일 생성 중..."
 echo "$DJANGO_ENV_PROD_CONTENTS" > /app/backend/.env.prod
 
-# ✅ Django 마이그레이션 및 static 파일 수집
+
 #python manage.py makemigrations --no-input
 python manage.py migrate --no-input
+
+# ✅ Django 마이그레이션 및 static 파일 수집
 python manage.py collectstatic --no-input #dev_6_2
+
+#dev_6_4 커맨드 객체 추가
+echo "Creating superuser..."
+python manage.py superuser || true
+echo "Superuser created"
+
 
 # ✅ 명령어 실행
 exec "$@"
